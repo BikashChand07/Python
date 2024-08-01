4c2b24c39c93bb37bf3f1c70c6bcfb358caab583
BikashChand07 <bc5588264@gmail.com>
2024-08-01 18:05:20 +0545

    if else basics

---
+++ Conditional statements/00_if_else.py
@@ -0,0 +1,13 @@
+#flow control
+#if...else..
+age=17
+if age < 18:
+    print("Unauthorized") #Indentation = 4 spaces or 1 tab
+
+print('\n')
+x=19
+if x>=18:
+    print("Authorized")
+    print("hi")
+
+print("hello")
