281b5249bb938be80107466dd8e9492dd685ef00
BikashChand07 <bc5588264@gmail.com>
2024-08-01 18:02:30 +0545

    list methods

---
+++ 08_List/02_list_methods.py
@@ -0,0 +1,8 @@
+#list methods
+x=[1,2,3,4]
+print(sum(x)) #add all the elements of the list
+print(min(x)) #returns the minimum from list
+print(max(x))
+print(len(x)) #find the number of elements in list or length of list
+print(sorted(x)) #sort the list in asscending order
+print(sorted(x, reverse=True)) #sort the list in descending order
