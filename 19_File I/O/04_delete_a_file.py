60ea7a184bae7185bd0472d12d781881f8d238dd
BikashChand07 <bc5588264@gmail.com>
2024-08-11 16:31:16 +0545

    delete a file

---
+++ 19_File I/O/04_delete_a_file.py
@@ -0,0 +1,3 @@
+#To delete a file we have to import Os module. Module is like a library or file written by another programmer that generally has a functions we can use
+import os
+os.remove("hello.txt")
