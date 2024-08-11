0868a8e467a90bf5f7f5a5dea595ef1e474c8263
BikashChand07 <bc5588264@gmail.com>
2024-08-11 16:22:38 +0545

    writing to a file

---
+++ 19_File I/O/03_write_in_File.py
@@ -0,0 +1,4 @@
+f=open("demo.txt","w")# open a file(demo.txt) in a writing mode. If demo.txt file doesnot exist then this will create a file named as demo.txt
+f.write("write mode overwrites the existing file")
+f.write("\n Thank You")
+f.close()
